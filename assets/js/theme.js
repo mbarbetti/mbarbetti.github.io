ca9b1eaa38f892b7c134f6ac5b6d2829837b6a1b
mbarbetti <55536905+mbarbetti@users.noreply.github.com>
2023-09-16 10:13:33 +0000

    Deploying to gh-pages from @ mbarbetti/mbarbetti.github.io@071747a2bfd2bf1a1e51334761840ec1e86cd640 🚀

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -12,15 +12,25 @@ let toggleTheme = (theme) => {
 let setTheme = (theme) =>  {
   transTheme();
   setHighlight(theme);
+  setGiscusTheme(theme);
 
   if (theme) {
     document.documentElement.setAttribute("data-theme", theme);
-  }
-  else {
+
+    // Add class to tables.
+    let tables = document.getElementsByTagName('table');
+    for(let i = 0; i < tables.length; i++) {
+      if (theme == "dark") {
+        tables[i].classList.add('table-dark');
+      } else {
+        tables[i].classList.remove('table-dark');
+      }
+    }
+  } else {
     document.documentElement.removeAttribute("data-theme");
   }
   localStorage.setItem("theme", theme);
-  
+
   // Updates the background of medium-zoom overlay.
   if (typeof medium_zoom !== 'undefined') {
     medium_zoom.update({
@@ -29,6 +39,7 @@ let setTheme = (theme) =>  {
     })
   }
 };
+
 
 let setHighlight = (theme) => {
   if (theme == "dark") {
@@ -41,6 +52,23 @@ let setHighlight = (theme) => {
 }
 
 
+let setGiscusTheme = (theme) => {
+
+  function sendMessage(message) {
+    const iframe = document.querySelector('iframe.giscus-frame');
+    if (!iframe) return;
+    iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
+  }
+
+  sendMessage({
+    setConfig: {
+      theme: theme
+    }
+  });
+
+}
+
+
 let transTheme = () => {
   document.documentElement.classList.add("transition");
   window.setTimeout(() => {
@@ -50,12 +78,13 @@ let transTheme = () => {
 
 
 let initTheme = (theme) => {
-  if (theme == null) {
+  if (theme == null || theme == 'null') {
     const userPref = window.matchMedia;
     if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
         theme = 'dark';
     }
   }
+
   setTheme(theme);
 }
 
